d1375960c34c48cd8829d3b8180d83f605650600
LuizAlbar <luizgustavo.ab@hotmail.com>
2025-11-11 17:13:38 -0300

    feat: add jwt authentication

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register.ts";
 import { authenticate } from "./controllers/authenticate.ts";
+import { profile } from "./controllers/profile.ts";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+  app.get("/me", profile)
 }
