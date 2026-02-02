20c636e92aff45f12d3b68958712df2f7ec4499a
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-02-02 09:34:15 -0300

    feat: set tests environments

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,43 @@
-import { defineConfig } from "vitest/config";
+import path from "node:path";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+	plugins: [tsconfigPaths({ root: "./" })],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	test: {
+		projects: [
+			{
+				plugins: [tsconfigPaths({ root: "./" })],
+				resolve: {
+					alias: {
+						"@": path.resolve(__dirname, "./src"),
+					},
+				},
+				test: {
+					name: "unit",
+					include: ["src/**/*.spec.ts"],
+					environment: "node",
+				},
+			},
+			{
+				plugins: [tsconfigPaths({ root: "./" })],
+				resolve: {
+					alias: {
+						"@": path.resolve(__dirname, "./src"),
+					},
+				},
+				test: {
+					name: "e2e",
+					include: ["src/http/controllers/**/*.test.ts"],
+					environment:
+						"./prisma/vitest-environment-prisma/prisma-test-environment.ts",
+				},
+			},
+		],
+	},
 });
